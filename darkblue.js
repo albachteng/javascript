0e3d39678b45fbc2f7825378362b241eb6990029
Graham Albachten <albachteng@gmail.com>
2021-01-20 12:00:05 -0600

    generate State class for game status, actor locations

--- darkblue.js
+++ darkblue.js
@@ -6,16 +6,33 @@ class Level {
         let rows = plan.trim().split("\n").map(l => [...l]); 
         // trim takes away whitespace at beginning and end, then we split at each newline
         // these lines are mapped to a spread array, creating each row of the level space
-        this.height = rows.length;
+        this.height = rows.length; // derived from "rows"
         this.width = rows[0].length;
-        this.startActors = [];
+        this.startActors = []; // moving elements
 
-        this.rows = rows.map((row,y) => {
+        this.rows = rows.map((row, y) => {
             let type = levelChars[ch];
-            if (typeof type == "string") return type;
+            if (typeof type == "string") return type; 
             this.startActors.push(
                 type.create(new Vec(x, y), ch));
                 return "empty";
         })
     }
 }
+
+class State {
+    constructor(level, actors, status) {
+        this.level = level;
+        this.actors = actors; // moving elements
+        this.status = status;
+    }
+
+    static start(level) {
+        return new State(level, level.startActors, "playing");
+    } // status will switch to "lost" or "won" when game is ended;
+
+    get player() {
+        return this.actors.find(a => a.type == "player"); // locate player
+    }
+} // persistent: updating game state creates a new state and leaves the old one intact
+
