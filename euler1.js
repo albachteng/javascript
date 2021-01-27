0e293167ed474dbe6d829288b6c66d588319caa1
Graham Albachten <albachteng@gmail.com>
2021-01-27 09:09:46 -0600

    add euler1.js

---
+++ euler1.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+const euler1 = function() {
+    let results = [],
+    // iterate through numbers zero through 1000
+    // push numbers divisible by 3 or 5 to an array
+    // reduce the array by summing them
+
+    for (let i = 0; i < 1000)
+
+}
