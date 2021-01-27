f056b07b463958d0d612c4f7747b475091a91c7e
Graham Albachten <albachteng@gmail.com>
2021-01-27 09:44:03 -0600

    finish euler2 sum even numbers up to 4mil

--- euler2.js
+++ euler2.js
@@ -9,12 +9,19 @@ const euler2 = function() {
     let numbers = [1, 2];
 // start with a brute force approach: calculate the next number
 // stop when the last array value is 4000000 or greater
-    for (let i = 0; array[i - 1] <= 4000000; i++) {
+    for (let i = 2; numbers[i - 1] < 4000000; i++) {
         // next number is results index a + resultx index a - 1
-        numbers[i] = numbers[i - 1] + numbers[i - 2];
+        numbers.push(numbers[i - 1] + numbers[i - 2]);
     }
-// if it is even, push to array
-
-// reduce the array
-
+// iterate through the fibonacci numbers: if it is even, push to array
+// initialize a counter
+    let counter = 0;
+    numbers.forEach(value => {
+        if (value % 2 == 0) {
+            counter+= value;
+        }
+    })
+    return counter; 
 }
+
+console.log(euler2());
