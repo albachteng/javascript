c032e17ac0aea3d95e631eadcd0d7e363708670b
Graham Albachten <albachteng@gmail.com>
2021-01-27 09:36:19 -0600

    begin euler2 sum even fibonacci numbers

---
+++ euler2.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+// this should likely involve memoization since there are so many terms...
+
+const euler2 = function() {
+    let numbers = [1, 2];
+// start with a brute force approach: calculate the next number
+// stop when the last array value is 4000000 or greater
+    for (let i = 0; array[i - 1] <= 4000000; i++) {
+        // next number is results index a + resultx index a - 1
+        numbers[i] = numbers[i - 1] + numbers[i - 2];
+    }
+// if it is even, push to array
+
+// reduce the array
+
+}
