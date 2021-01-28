55c67352434ec746277e8ad5b1d464f3df7cb322
Graham Albachten <albachteng@gmail.com>
2021-01-28 09:51:26 -0600

    add isPrime helper function and identify problem spots

--- euler3.js
+++ euler3.js
@@ -13,12 +13,19 @@
 
 const factor = 600851475143;
 
+const isPrime = num => { // helper function checks for prime
+    for (let i = 2; i < Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    return num > 1;
+}
+
 for (
     let i = Math.ceil(factor / 2); // begin testing for factors at halfway point since it is not possible for a factor to have a pair greater than 2
     i > Math.sqrt(factor); // stop testing for factors at the square root, since any factors smaller than the square root would have to be paired with a number greater than the square root and already tested for
     i-= 2) { // step down by two (no need to test even numbers as they are not prime)
         if (factor % i === 0) { // if factor divides evenly by i, i is a factor and needs to be tested for prime
-            console.log(`Testing factor ${i} for prime`);
+            console.log(`Testing factor ${i} for prime`); // flag is never printed to console, so it is the above logic that is flawed;
             for (let j = 2; j < Math.sqrt(i); i++) {// test for prime by modulo == 0 starting at 2 and up to sqrt(i)
                 if (i % j === 0) { // if i can be divided evenly by any number up to its square root, it is not prime
                     break;  // as soon as we find that it is not prime, we can break out of the inner loop
