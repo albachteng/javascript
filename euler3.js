431a1da0b0915e0f68f5c705560d669ddd9673ab
Graham Albachten <albachteng@gmail.com>
2021-01-28 09:07:02 -0600

    add failed attempt at prime number factorization problem

--- euler3.js
+++ euler3.js
@@ -3,3 +3,13 @@
 // probably want to start at the halfway point and iterate downward, since the largest possible factor will be half the number
 // can use the modulo operator to determine if a number is a factor
 // but then comes the difficulty of determining if the number is prime... 
+// for this we can generate an array of prime numbers using the sieve approach
+
+const factor = 600851475143;
+let primes = []; 
+
+for (let i = 0; i < factor / 2; i++) {
+    primes.push(i); // fill our primes array with all integers up to the halfway point
+} 
+
+console.log(primes[5]); // failed, out of memory, not a good solution, over 300 billion items
