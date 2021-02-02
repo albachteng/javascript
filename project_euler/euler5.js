461f15188caa1173900653f400b1c2689b22de13
Graham Albachten <albachteng@gmail.com>
2021-02-02 14:08:09 -0600

    starting a different approach to factorizing

--- project_euler/euler5.js
+++ project_euler/euler5.js
@@ -6,18 +6,23 @@ all of the numbers from 1 to 20? */
 // but that is an astronomical number, so there is doubtless a much smaller solution 
 // also many of the numbers 1 - 20 share factors; all even numbers are divisible by 2 for example
 // so what we really need to know is what number is the product of all unique factors of the numbers
-// in range from 1-20
+// in range from 1-20 (1 * 2 * 3 * 4 * 5 * 7 = 210) hmm
+// that doesn't actually work because 210 is not divisible evenly by 4, 8, 9 
+// to resolve the discrepency, I noticed we can multiply by 2 again to make it divisible by 4
+// multiply it by 2 again to make it divisible by 8 (2^3 = 8)
+// multiply it by 3 again to make it divisible by 9 (3^2 = 9)
+
 // so let's write code to factor a number
 
 const factor = (num) => {
     let factors = []; 
-    for (i = 3; i < num; i+= 2) { // start at 3 since we can easily assume that 2 is a factor and ignore all even numbers
+    for (i = 2; i < num; i+= 1) { // start at 2
         if (num % i === 0) {
-            factors.push(i);
+            factors.push(i, num);
+            num /= i;
         }
     }
     return factors;
 }
 
-console.log(factor(20)); // ignoring 2, which is easy to identify as a factor, we expect 5;
-// since 2 * 2 * 5 = 20, we have identified all factors
+console.log(factor(20)); 
