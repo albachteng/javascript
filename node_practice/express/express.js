6aa0d9b741276863b0e061e7a70f61b24b1eae0d
Graham Albachten <albachteng@gmail.com>
2021-02-04 09:34:54 -0600

    continue with learnyounode, add make-it-modular.js and mymodule.js

--- node_practice/express/express.js
+++ node_practice/express/express.js
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static('express')); // static files in 'public' folder
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
@@ -12,4 +12,9 @@ app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
     // and PATH is the part of the request URL after hostname and port number
     // in express, app.get() = method to register routes to match GET requests
 
-// app.get(path, callback); 
+// const moods = [{ mood: 'excited'}, { mood: 'blahblah'}]; 
+// app.get('/moods', (req, res, next) => {
+    // response block
+// }); 
+// req, res (request and response objects), next???
+
