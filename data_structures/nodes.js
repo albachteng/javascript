5c26808f6c7e965b86a8049773d176a20d2837e7
Graham Albachten <albachteng@gmail.com>
2021-03-02 08:40:19 -0600

    add seasons LinkedList

--- data_structures/nodes.js
+++ data_structures/nodes.js
@@ -29,5 +29,61 @@ let oldestName = '';
 
 while (current !== null) {
     oldestName = current.data;
-    
+    current = current.getNextNode();
 }
+
+console.log(oldestName); // expect 'oldest'
+
+class LinkedList {
+    constructor() {
+        this.head = null; // initialize empty
+    }
+
+    addToHead(data) {
+        const newHead = new Node(data);
+        const currentHead = this.head;
+        this.head = newHead;
+        if (currentHead) {
+            this.head.setNextNode(currentHead);
+        }
+    }
+
+    addToTail(data) {
+        let current = this.head;
+        if (!current) {
+            this.head = new Node(data);
+        } else {
+            while (current.getNextNode() !== null) {
+                current = current.getNextNode();
+            }
+            current.setNextNode(new Node(data));
+        }
+    }
+
+    removeHead() {
+        const removedHead = this.head;
+        if (!removedHead) {
+            return;
+        }
+        this.head = removedHead.getNextNode();
+        return removedHead.data;
+    }
+
+    printList() {
+        let currentNode = this.head;
+        let output = '<head> ';
+        while (currentNode !== null) {
+            output += currentNode.data + ' ';
+            currentNode = currentNode.getNextNode();
+        }
+        output += '<tail>';
+        console.log(output); 
+    }
+}
+
+const list = new LinkedList();
+let seasons = ['spring', 'summer', 'fall', 'winter', 'spring'];
+for (season in seasons) {
+    list.addToTail(seasons[season]); 
+}
+list.printList();
