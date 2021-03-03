8838493a5b91dec3b0c3d7827cd23fe9f6909353
Graham Albachten <albachteng@gmail.com>
2021-03-03 12:34:05 -0600

    add search function

--- data_structures/hash.js
+++ data_structures/hash.js
@@ -1,6 +1,6 @@
 class HashTable {
     constructor() {
-        this.values = {};
+        this.values = {}; 
         this.length = 0;
         this.size = 0; 
     }
@@ -11,13 +11,21 @@ class HashTable {
 
     add(key, value) {
         const hash = this.calculateHash(key); 
-        if (!this.values.hasOwnProperty(hash)) { // if it doesn't already have that hash
+        if (!this.values.hasOwnProperty(hash)) { // if values doesn't already have that hash
             this.values[hash] = {}; // add the hash as a key to values and initialize to empty object
         }
         if (!this.values[hash].hasOwnProperty(key)) {
-            this.length++; 
+            this.length++; // if the provided key doesn't already exist at that hash
+        } // we can increment the length to add it
+        this.values[hash][key] = value; // finally we add the key value pair into the hash object
+    }
+
+    search(key) {
+        const hash = this.calculateHash(key);
+        if (this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
+            return this.values[hash][key];
+        } else {
+            return null;
         }
-        this.values[hash][key] = value;
-        // values get added within the object associated with the hash as a key-value pair
     }
 }
