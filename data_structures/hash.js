a4e456f5f9e4e57eeea50315e8f50a5cf582b55b
Graham Albachten <albachteng@gmail.com>
2021-03-03 12:29:40 -0600

    implement add function

--- data_structures/hash.js
+++ data_structures/hash.js
@@ -8,4 +8,16 @@ class HashTable {
     calculateHash(key) {
         return key.toString().length % this.size;
     }
+
+    add(key, value) {
+        const hash = this.calculateHash(key); 
+        if (!this.values.hasOwnProperty(hash)) { // if it doesn't already have that hash
+            this.values[hash] = {}; // add the hash as a key to values and initialize to empty object
+        }
+        if (!this.values[hash].hasOwnProperty(key)) {
+            this.length++; 
+        }
+        this.values[hash][key] = value;
+        // values get added within the object associated with the hash as a key-value pair
+    }
 }
