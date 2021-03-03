77c37345d498c264789fe6b5108b50b88664d49d
Graham Albachten <albachteng@gmail.com>
2021-03-03 12:41:19 -0600

    trying out a different implementation using bucket chaining

--- data_structures/hash.js
+++ data_structures/hash.js
@@ -5,7 +5,7 @@ class HashTable {
         this.size = 0; 
     }
 
-    calculateHash(key) {
+    calculateHash(key) { // simple arithmetic modular hash function
         return key.toString().length % this.size;
     }
 
@@ -24,8 +24,28 @@ class HashTable {
         const hash = this.calculateHash(key);
         if (this.values.hasOwnProperty(hash) && this.values[hash].hasOwnProperty(key)) {
             return this.values[hash][key];
+// remember that we have to check to make sure the hash AND the key exist, since a hash could contain a number of key-value pairs
         } else {
             return null;
         }
+    }
+}
+
+class HashEntry {
+    constructor(key, data) {
+        this.key = key; 
+        this.value = data;
+        this.next = null;
+    }
+}
+
+class HashTableTwo {
+    constructor() {
+        this.slots = 10;
+        this.size = 0;
+        this.bucket = [];
+        for (let i = 0; i < this.slots; i++) {
+            this.bucket[i] = null; 
+        } // initialize all buckets for the given number of slots to null
     }
 }
