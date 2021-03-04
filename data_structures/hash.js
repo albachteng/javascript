b60f354922888a875bfdd3dd1d21a1491213381d
Graham Albachten <albachteng@gmail.com>
2021-03-04 10:20:52 -0600

    add double method to increase the size of the hashtable as it grows

--- data_structures/hash.js
+++ data_structures/hash.js
@@ -55,15 +55,35 @@ class HashTableTwo {
         return this.getSize() == 0;
     }
     getIndex(key) {
-        let index = key % this.slots;
+        let index = key.toString().length % this.slots;
         return index;
     }
+    double() {
+        // doubles slots when size reaches 60% of slots 
+        this.slots*= 2;
+        console.log(this.slots);
+        let oldBucket = this.bucket;
+        let newBucket = [];
+        for (let i = 0; i < this.slots; i++) {
+            newBucket[i] = null; 
+        } // create a newBucket of the appropriate size
+        this.bucket = newBucket;
+        oldBucket.forEach(index => {
+            if (index) {
+                Object.keys(index).forEach(key => {
+                    this.add(key, index[key]);
+                })
+        }})
+    }
     add(key, value) {
+        if (this.size >= .6 * this.slots) {
+            this.double();
+        }
         const hash = this.getIndex(key);
         if (!this.bucket[hash]) {
             this.bucket[hash] = {};
         }
-        if (!this. buckets[hash].hasOwnProperty(key)) {
+        if (!this.bucket[hash].hasOwnProperty(key)) {
             this.size++;
         }
         this.bucket[hash][key] = value;
@@ -79,9 +99,35 @@ class HashTableTwo {
     delete(key) {
         const hash = this.getIndex(key); 
         if (this.bucket[hash] && this.bucket[hash].hasOwnProperty(key)) {
-            delete this.bucket[hash][key];
+            delete this.bucket[hash][key]; // should return true
+            if (this.bucket[hash] && Object.keys(this.bucket[hash]).length === 0) {
+                this.bucket[hash] = null; 
+            }
         } else {
             return null;
         }
     }
 }
+
+const peopleList = [{name: 'bob', age: 12}, 
+                    {name: 'sally', age: 18}, 
+                    {name: 'constance', age: 32}, 
+                    {name: 'george', age: 35}, 
+                    {name: 'sam', age: 15},
+                    {name: 'harry', age: 22},
+                    {name: 'bill', age: 5},
+                    {name: 'peter', age: 16},
+                    {name: 'luis', age: 54},
+                    {name: 'gustavo', age: 19},
+                    {name: 'gilgamesh', age: 20}];
+const peopleHash = new HashTableTwo;
+console.log(peopleHash.isEmpty()); // expect true
+for (person of peopleList) {
+    peopleHash.add(person.name, person.age);
+}
+console.log(peopleHash);
+console.log(peopleHash.getSize(), peopleHash.isEmpty()); // 11, false
+console.log(peopleHash.search('gustavo')); // 19 (his age)
+peopleHash.delete('constance'); 
+console.log(peopleHash.search('constance')); // null
+console.log(peopleHash);
